3030eb84844068510f058bf9af66fabd9255fea8
Herald Carl Avila <heavila@my.cspc.edu.ph>
2024-08-16 19:58:47 +0800

    My first commit

---
+++ Onate_JosephJessie/PS1/avila_heraldcarl_ps1/android/app/src/main/kotlin/com/example/avila_heraldcarl_ps1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.avila_heraldcarl_ps1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Onate_JosephJessie/PS1/avila_heraldcarl_ps1/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main() {
+  // logger.info("Hello World");
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello World")
+        )
+      )
+    )
+  );
+}
