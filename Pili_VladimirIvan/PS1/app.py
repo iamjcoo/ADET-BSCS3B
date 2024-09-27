8167401b5052dea33b378d1f31fb5048925044ef
blad-imir <vlpili@my.cspc.edu.ph>
2024-09-27 12:11:52 +0800

    PS1 and PS2

--- Pili_VladimirIvan/PS1/app.py
+++ Pili_VladimirIvan/PS1/app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
-    if request.method == "POST":
-        name = request.form.get("name")
-    return render_template("index.html", name=name)
+    if request.method == 'POST':
+        name = request.form.get('name')
+        if name:
+            return render_template('index.html', greeting=f'Hello, {name}!')
+    return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '_main_':
     app.run(debug=True)
