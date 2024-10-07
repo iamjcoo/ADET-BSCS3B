bc69f8e51e8310e0e00e97a143f3a6050b5b2fae
Franco Aurellano <diaurellano@my.cspc.edu.ph>
2024-10-07 20:14:23 +0800

    Added PS2(open for configure)

--- Aurellano_DivinoFranco/PS2/app.py
+++ Aurellano_DivinoFranco/PS2/app.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import json
+import json
+import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def hello_world():
-    if request.method == "POST":
-        name = request.form.get("name")
-        return render_template("index.html", title="Hello", name=name)
-    return render_template("index.html", title="Hello")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    info = request.form.to_dict()
+    
+    data = {
+    "firstName": info['f_name'], 
+    "middleName": info['m_name'], 
+    "lastName": info['l_name'], 
+    "contactNumber": info['c_number'], 
+    "emailAddress":info['email'], 
+    "address": info['address'], 
+}
+
+    # Save data to JSON file
+    with open('data.json', 'a') as f:
+        json.dump(data, f)
+        f.write('\n')  # Add a newline for better readability
+
+    return jsonify({'message': 'Information saved successfully!'})
 
 if __name__ == "__main__":
-    app.run(debug=True)  
+    app.run(debug=True)
